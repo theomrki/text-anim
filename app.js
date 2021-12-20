e2273d8fb24b1825a0ae13e141c65f7a47a4a2a8
theomrki <mouaraki.dev@gmail.com>
2021-12-20 15:47:50 +0100

    .

---
+++ app.js
@@ -0,0 +1,37 @@
+const target = document.getElementById("target");
+let array = ["developer", "photographer", "geek"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2000);
+    }
+  }, 150);
+};
+
+loop();
